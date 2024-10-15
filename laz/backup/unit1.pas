29a95e085aa42c64fe45a9355a75f48c04ae6b49
chebpng <yota3301@gmail.com>
2024-10-15 15:36:41 +0700

    1

---
+++ laz/backup/unit1.pas
@@ -0,0 +1,43 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Image1: TImage;
+    Label1: TLabel;
+    procedure Button2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  close;
+end;
+
+end.
+
---
+++ беск суммы.pas
@@ -0,0 +1,11 @@
+﻿var eps, sum, f, x: real;
+       k: integer;
+begin
+   f := x; sum := f; k := 2;
+   while abs(f) >= eps do
+     begin
+       f := k * (x ** k);
+       sum := sum + f;
+       k := k + 1;
+    end;
+end.
